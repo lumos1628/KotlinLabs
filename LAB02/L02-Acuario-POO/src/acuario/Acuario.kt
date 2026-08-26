8f9a0ef85575d941d6a00ed537087dce46443e74
gabriel-l <lumos2719@gmail.com>
2026-08-26 17:23:46 -0500

    bloques de inicializacion, un init se ejecutamente automaticamente y puede mostrar algun proceso algun comando o funcion predefinida, en este caso se imprime el volumen del acuario usando una formula y una sintaxis interesante  con variables

--- LAB02/L02-Acuario-POO/src/acuario/Acuario.kt
+++ LAB02/L02-Acuario-POO/src/acuario/Acuario.kt
@@ -7,5 +7,13 @@ class Acuario(largo: Int =100, ancho: Int = 20, alto: Int = 40) {
 
     fun imprimirTamano() {
         println("Ancho: $ancho cm " + "Largo: $largo cm" + "alto: $alto cm")
+
+    }
+    init {
+        println("Iniciando acuario")
+    }
+    init {
+        // i litro = 1000 cm3
+        println("Volumen: ${ancho*largo*alto/1000}l")
     }
 }
