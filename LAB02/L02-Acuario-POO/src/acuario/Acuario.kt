821d46498bd9fe4c920d6949136040c8c6a126d4
gabriel-l <lumos2719@gmail.com>
2026-08-26 17:35:53 -0500

    constructores secundarios -> se puede crear un constructor secundario, este crea nuevos atributos, asi mismo las funciones creadas dentro de este pueden cambiar el valor de los atributos de la clase, en ele ejmplo se cambia el alto

--- LAB02/L02-Acuario-POO/src/acuario/Acuario.kt
+++ LAB02/L02-Acuario-POO/src/acuario/Acuario.kt
@@ -1,12 +1,20 @@
 package acuario
 
+import kotlin.div
+import kotlin.text.toInt
+
 class Acuario(largo: Int =100, ancho: Int = 20, alto: Int = 40) {
     var ancho:Int = largo
     var alto:Int = ancho
     var largo:Int = alto
 
+    constructor(numeroDePeces: Int): this(){
+        val tanque = numeroDePeces * 2000 * 1.1
+        alto = (tanque / (largo * ancho)).toInt()
+    }
+
     fun imprimirTamano() {
-        println("Ancho: $ancho cm " + "Largo: $largo cm" + "alto: $alto cm")
+        println("Tamaño -> Ancho: $ancho cm " + "Largo: $largo cm" + "alto: $alto cm")
 
     }
     init {
--- LAB02/L02-Acuario-POO/src/acuario/main.kt
+++ LAB02/L02-Acuario-POO/src/acuario/main.kt
@@ -17,6 +17,11 @@ fun construirAcuario(){
     // todo personalizado
     val acuario4 = Acuario(ancho = 25, alto = 35, largo = 110)
     acuario4.imprimirTamano()
+
+    val acuario6 = Acuario(numeroDePeces = 29)
+    acuario6.imprimirTamano()
+    println("Volumen: ${acuario6.ancho * acuario6.largo * acuario6.alto / 1000}l")
+
 }
 
 fun main() {
