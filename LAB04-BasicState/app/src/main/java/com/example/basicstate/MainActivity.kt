bf2ca5b3b34e50c99c377953c8e1b70a4b8e83de
gabriel-l <lumos2719@gmail.com>
2026-09-15 23:26:57 -0500

    creando proyecto de estados

---
+++ LAB04-BasicState/app/src/main/java/com/example/basicstate/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.basicstate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.basicstate.theme.BasicStateTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    enableEdgeToEdge()
+    setContent {
+      BasicStateTheme { Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) { MainNavigation() } }
+    }
+  }
+}
